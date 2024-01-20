01c3d88142b07bbc388fd944cc2b085ff01aa1d8
Kardfox01 <kardfox.i@gmail.com>
2024-01-20 23:47:31 +0200

    Полностью реализована коллизия; Упразднена система секторов; Добавлена интерпретация команд из командной строки.

--- main.py
+++ main.py
@@ -1,81 +1,122 @@
+import pygame as pg
 from particle import Particle
 from coordinates import Coordinates
-import pygame as pg
-
-from sectors import Sectors
-
+from threading import Thread
+from random import randint as rd
 
 
 class BrownianMotion:
     class Parameters:
-        # WINDOW
+        # Окно
         CAPTION = "Brownian motion"
         SIDE    = 900
         ICON    = "icon.ico"
         FILL    = (255, 255, 255)
 
-        # APP
-        N = 3
-
-        # PHYSICS
-        V    = 5
-        MASS = 5
-        SIZE = 10
+        # Физика
+        V      = 10
+        MASS   = 100
+        RADIUS = 20
 
 
     def __init__(self):
         pg.init()
 
         self.screen: pg.Surface = pg.display.set_mode((self.Parameters.SIDE, self.Parameters.SIDE))
+        self.particles: list[Particle] = []
+        self.running = True
 
         pg.display.set_caption(self.Parameters.CAPTION)
         pg.display.set_icon(pg.image.load(self.Parameters.ICON))
 
-        self.particles: list[Particle] = []
-        self.sectors  : Sectors        = Sectors(self.Parameters.N, self.Parameters.SIDE)
-
     def run(self):
-        running = True
-        clock   = pg.time.Clock()
-    #     sources: list[Source] = []
+        clock = pg.time.Clock()
 
-        while running:
+        while self.running:
             self.screen.fill(self.Parameters.FILL)
 
-    #         for source in sources:
-    #             if not source.reflected:
-    #                 pg.draw.circle(
-    #                     self.screen,
-    #                     (0, 255, 255),
-    #                     source.center,
-    #                     10
-    #                 )
-    #             source.draw()
-
-            for event in (event for event in pg.event.get() if event.type in (pg.QUIT, pg.MOUSEBUTTONUP)):
+            for event in pg.event.get():
                 if event.type == pg.QUIT:
-                    running = False
+                    self.stop()
 
                 elif event.type == pg.MOUSEBUTTONUP and event.button == 1:
                     coordinates = Coordinates(*pg.mouse.get_pos())
-                    self.particles.append(
-                        Particle(
-                            self.Parameters.SIZE,
-                            self.Parameters.MASS,
-                            self.Parameters.V,
-                            self.sectors.where(coordinates),
-                            coordinates
-                        )
-                    )
+                    self.add(1, coordinates)
 
             for particle in self.particles:
-                pg.draw.circle(self.screen, *particle.get)
+                particle.check_collision(self.particles, self.Parameters.SIDE)
+                pg.draw.circle(self.screen, *particle.figure())
 
             pg.display.update()
             clock.tick(75)
         pg.quit()
 
+    def add(self, N: int, coordinates: Coordinates | None = None):
+        for _ in range(N):
+            self.particles.append(
+                Particle(
+                    self.Parameters.RADIUS,
+                    self.Parameters.MASS,
+                    rd(-self.Parameters.V, self.Parameters.V),
+                    rd(-self.Parameters.V, self.Parameters.V),
+                    coordinates or Coordinates(
+                        rd(0, self.Parameters.SIDE - self.Parameters.RADIUS*2),
+                        rd(0, self.Parameters.SIDE - self.Parameters.RADIUS*2)
+                    )
+                )
+            )
+
+    def create(
+        self,
+        radius : int,
+        mass   : int,
+        Vx     : int,
+        Vy     : int,
+        color_r: int,
+        color_g: int,
+        color_b: int,
+        x      : int | None = None,
+        y      : int | None = None,
+    ):
+        self.particles.append(
+            Particle(
+                radius,
+                mass,
+                Vx or rd(-self.Parameters.V, self.Parameters.V),
+                Vy or rd(-self.Parameters.V, self.Parameters.V),
+                Coordinates(
+                    x or rd(0, self.Parameters.SIDE - self.Parameters.RADIUS*2),
+                    y or rd(0, self.Parameters.SIDE - self.Parameters.RADIUS*2)
+                ),
+                (color_r, color_g, color_b)
+            )
+        )
+
+    def stop(self):
+        self.running = False
+
+    def command_center(self):
+        commands = {
+            "добавить": self.add,                           # добавить N                          - добавляет N обычных частиц
+            "создать" : self.create,                        # создать RADIUS MASS VX VY R G B X Y - добавляет 1 частицу с заданными параметрами
+            "очистить": self.particles.clear,               # очистить                            - удаляет все частицы
+            "кол-во"  : lambda: print(len(self.particles)), # кол-во                              - выводит кол-во частиц на экране
+            "стоп"    : self.stop                           # стоп                                - останавливает приложение
+        }
+
+        while self.running:
+            promt = input().split()
+            command, args = promt[0], (int(arg) for arg in promt[1:])
+            try:
+                commands[command](*args)
+            except KeyError:
+                print("Такой команды не существует")
+            except Exception as e:
+                print(e)
+        print("Выход...")
+
 
 if __name__ == "__main__":
     bm = BrownianMotion()
+    Thread(target=bm.command_center).start()
     bm.run()
--- particle.py
+++ particle.py
@@ -1,17 +1,65 @@
-from dataclasses import dataclass
-from coordinates import Coordinates
-from sectors import Sector
+from __future__ import annotations
+
+from coordinates import Coordinates, dataclass
 
 
 @dataclass
 class Particle:
-    size       : int
-    mass       : int
-    v          : int
-    sector     : Sector
-    coordinates: Coordinates
-    color      : tuple[int, int, int] = (0, 0, 0)
+    radius  : int
+    m       : int
+    Vᵪ      : int
+    Vᵧ      : int
+    coords  : Coordinates
+    color   : tuple[int, int, int] = (0, 0, 0)
+    collided: bool                 = False
 
-    @property
-    def get(self) -> tuple[tuple[int, int, int], tuple[int, int], int]:
-        return self.color, self.coordinates.get, self.size
+    def figure(self) -> tuple[tuple[int, int, int], tuple[int, int], int]:
+        self.coords.x += int(self.Vᵪ)
+        self.coords.y -= int(self.Vᵧ)
+
+        self.collided = False
+
+        return self.color, self.coords.get, self.radius
+
+    def check_collision(self, particles: list[Particle], side: int):
+        if (
+            self.coords.x - self.radius < 0    and self.Vᵪ < 0 or
+            self.coords.x + self.radius > side and self.Vᵪ > 0
+        ):
+            self.Vᵪ *= -1
+
+        if (
+            self.coords.y - self.radius < 0    and self.Vᵧ > 0 or
+            self.coords.y + self.radius > side and self.Vᵧ < 0
+        ):
+            self.Vᵧ *= -1
+
+        for another in particles:
+            if (
+                self >> another <= self.radius + another.radius and
+                not self is another and
+                not self.collided
+            ):
+                _Vᵪ = self.Vᵪ
+                _Vᵧ = self.Vᵧ
+
+                self.Vᵪ = (2*another.m*another.Vᵪ + self.Vᵪ*(self.m - another.m)) // (self.m + another.m)
+                self.Vᵧ = (2*another.m*another.Vᵧ + self.Vᵧ*(self.m - another.m)) // (self.m + another.m)
+
+                another.Vᵪ = (2*self.m*_Vᵪ + _Vᵪ*(another.m - self.m)) // (self.m + another.m)
+                another.Vᵧ = (2*self.m*_Vᵧ + _Vᵧ*(another.m - self.m)) // (self.m + another.m)
+
+                self.coords.x += int(self.Vᵪ)
+                self.coords.y -= int(self.Vᵧ)
+
+                another.collided = True
+
+    def __rshift__(self, another: Particle) -> float:
+        distance = (
+            (another.coords.x - self.coords.x)**2 + 
+            (another.coords.y - self.coords.y)**2
+        )**.5
+
+        if type(distance) != complex:
+            return distance
+        return 0
